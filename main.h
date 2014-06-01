a76bd4c24d2c5f2050d118bb7bf1f6d59db227c9
pedro596 <sousapedro596@gmail.com>
2014-06-01 19:48:31 +0100

    pin defines

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#endif
+
+
+
+/* Define 1284p pins */
+
+#define RDY_BSY PB1
+#define OE		PB2
+#define WR		PB3
+#define XA0		PD0
+#define XA1		PD1
+#define PAGEL	PD2
+
+#define BS1		PB4
+#define BS2		PD3
+#define XTAL1	PD4
+
+#define DATA	PORTA
+#define DATAD	DDRA	
+
+
